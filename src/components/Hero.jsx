d53c2995f5a877c859546633f76bb178e842cb4a
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-17 23:22:09 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className=' p-4'>
+        <h1 className='text-5xl ' >
+            Celestia
+        </h1>
+        <h2 className='text-3xl'>
+         Sail into Serenity with Celestia
+        </h2>
+  </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <ul className=' flex gap-20 justify-start text-blue-700'>
+            <li>
+                <a href="" className=''> Home </a>
+            </li>
+            <li>
+                <a href=""> About</a>
+            </li>
+            <li>
+                <a href=""> Contact</a>
+            </li>
+            <li>
+                <a href=""></a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
