46f602ad69c791694c3fb01223c1549aaccd8117
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-18 01:18:56 +0530

    added carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,11 +3,26 @@ import React from 'react'
 
 function Carousel() {
   return (
-    <div className=' my-2 p-4 bg-slate-200'>
-        <h1 className=''>
-            Facities
-        </h1>
-        <img className='w-1/3 mx-auto' src="src\assets\food2.png" alt="" />
+    <div className=' p-4   '>
+        
+        <div className='grid grid-cols-1 bg-gradient-to-b from-blue-500 to-sky-200 md:grid-cols-2 items-center justify-center mx-auto p-8 gap-4 rounded-md '>
+            {/* Image Section */}
+            <div className='flex justify-center'>
+                <img 
+                    className='w-3/4 md:w-2/3 rounded-md' 
+                    src="src/assets/food2.png" 
+                    alt="Gourmet Meal"
+                />
+            </div>
+            
+            {/* Text Section */}
+            <div className='flex justify-center items-center text-center'>
+                <h3 className='text-3xl italic playfair-display  font-medium '>
+                    Indulge in gourmet meals prepared by world-class chefs, tailored to your tastes
+                </h3>
+            </div>
+        </div>
+
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className=' p-4 '>
-        <h1 className='text-5xl poppins my-3' >
+    <div className=' p-4  '>
+        <h1 className='text-5xl playfair-display my-3' >
             Celestia
         </h1>
-        <h2 className='ml-10 text-6xl  playfair-display italic my-3'>
+        <img className=' w-1/2 mx-auto ' src="src\assets\cruise2.png" alt="" />
+        
+        <h2 className='ml-10 text-6xl playfair-display  italic my-3'>
          Sail into Serenity with Celestia
         </h2>
   </div>
