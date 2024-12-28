125e0a6f26badcfeebbafbe2af64a39f8f6fb65e
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-29 00:27:11 +0530

    added backend test server of express

---
+++ BackEnd/app.js
@@ -0,0 +1,17 @@
+// const admin = require('firebase-admin')
+const express = require('express')
+const bodyparser = require('body-parser')
+
+const port = 5000
+
+const app = express()
+
+app.use(bodyparser.json())
+
+app.get('/', async (req, res) =>{
+    res.send('Backend is running!..')
+})
+
+app.listen(port, () =>{
+    console.log('Server running on port : 5000');
+})
