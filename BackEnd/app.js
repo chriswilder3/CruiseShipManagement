a9720743d47f0cb8c8234c89519ae6d4aeb68add
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-29 17:27:38 +0530

    added sample user check and navigate funcn

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,12 +7,30 @@ const port = 5000
 
 const app = express()
 
+app.use( cors(
+    { 
+        origin : 'http://localhost:5173',   
+    }
+))
 
+app.use( bodyparser.urlencoded({ extended : true}))
+// Used for accepting form data
 
 app.use(bodyparser.json())
 
 app.get('/', async (req, res) =>{
-    res.send('Backend is running!..')
+    
+       
+    res.send({
+        message : 'backend is running'
+        
+    })
+})
+
+app.post('/', async( req, res) => {
+    const {user,pass} =  req.body 
+
+    res.status(200).send(` User received details : ${user} , ${pass} `)
 })
 
 app.listen(port, () =>{
--- FrontEnd/src/components/users/Signin.jsx
+++ FrontEnd/src/components/users/Signin.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate} from 'react-router-dom'
 
 function Signin() {
     
+    const [errorMsg, setErrorMsg] = useState()
+    const navigate = useNavigate()
 
-    const handleSignIn = function (e) {
+    const handleSignIn = async function (e) {
         e.preventDefault()
-        console.log('Submit happening!');
+        const formData = new FormData(e.target)
+        const user = formData['user']
+        const pass = formData['pass']
+        console.log('Submit happening! : ', user, ' ', pass);
 
         const backEndUrl = 'http://localhost:5000/'
-        fetch(backEndUrl)
-        .then( (res) =>{
-            return res.json()
-        })
-        .then( (res) =>{
-            console.log(res);
-        })
+
+        try{
+            const response = await fetch(backEndUrl,
+                {
+                    method : 'post',
+                    headers : {
+                        'Content-Type': 'application/json'
+                    },
+                    body : JSON.stringify({user, pass})
+                })
+
+            if(response.ok ){
+                const data = await response.json()
+                console.log('Login Successful : ', data);
+                navigate('/dashboard')
+            }
+            else{
+                const errData = await response.json()
+                setErrorMsg(errData.message || 'Login failed')
+            }
+        }
+        catch( error){
+            console.error('Error during sign-in:', error);
+            setErrorMsg('An error occurred. Please try again later.');
+        }
+
     }
     
   return (
@@ -22,9 +47,11 @@ function Signin() {
         <h1>
             Sign into your account
         </h1>
-        <form action="" method='get' onSubmit={handleSignIn} className='signin-form flex flex-col gap-4 *:p-3 '>
-            <input type="text" placeholder=' Enter your username'/>
-            <input type="password" name="" id="" placeholder='Enter your password'/>
+        {/* onSubmit={handleSignIn} */}
+         {errorMsg && <p className='text-red-400 text-sm'> {{errorMsg}} </p> }
+        <form action="" method='post' onSubmit={handleSignIn}  className='signin-form flex flex-col gap-4 *:p-3 '>
+            <input type="text" name="user" placeholder=' Enter your username'/>
+            <input type="password" name="pass" id="" placeholder='Enter your password'/>
             <button type='submit' className=' bg-sky-800  rounded-md text-white'>
                 Submit
             </button>
