6ac5c890178a3064204f21811f1ddfd3685c3440
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-31 01:46:36 +0530

    fetch items from DB in catering services.

--- FrontEnd/src/components/services/Services.jsx
+++ FrontEnd/src/components/services/Services.jsx
@@ -1,60 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Services() {
+  
   return (
-    <>
-        <h1 className='text-5xl font-semibold poppins text-blue-600'>
-            Services Available:
-        </h1>
-        
-        <div className=' my-4 flex md:flex-row md:gap-36 flex-wrap justify-center poppins'>
-            <div className='flex flex-col rounded-md p-4'>
-                <h1 className='text-2xl font-semibold my-1 px-4 text-left ml-2 text-slate-700'>
-                    Catering:
-                </h1>    
-                <Link to="/services/catering"> <img className='w-72 my-1 mb-3 hover:scale-105  rounded-md ' src="src\assets\food.png" alt="" /> </Link>
-                <div className='flex text-lg flex-col gap-2 font-semibold text-white  *:min-w-40 *:bg-orange-300 *:rounded-tr-lg *:rounded-tl-lg *:p-1 *:shadow-md *shadow-gray-200  '>
-                    <p className='hover:scale-105'> <Link to="/services/catering/snacks">  Snacks </Link> </p> 
-                    <p className='hover:scale-105'> <Link to="/services/catering/beverages">Beverages </Link> </p> 
-                    <p className='hover:scale-105'> <Link to="/services/catering/desserts">Desserts  </Link> </p> 
-                    <p className='hover:scale-105'> <Link to="/services/catering/dining">Fine Dining </Link> </p> 
-                    <p className='hover:scale-105'> <Link to="/services/catering">Browse Menu </Link> </p> 
-                </div>
-            </div>
-
-            <div className='flex flex-col rounded-md p-4'>
-                <h1 className='text-2xl font-semibold my-1 px-4 text-left ml-2 text-slate-700'>
-                    Stationery:
-                </h1>
-                <Link to='/services/stationary' ><img className='w-72 my-1 rounded-md hover:scale-105 mb-3' src="src\assets\stationary.png" alt="" /> </Link>    
-                <div className='flex text-lg flex-col gap-2 font-semibold text-slate-100  *:min-w-40 *:bg-blue-400 *:rounded-tr-lg *:rounded-tl-lg *:p-1 *:shadow-md *shadow-gray-200 '>
-                    <p className='hover:scale-105'> <Link to='/services/stationary/gifts' > Gifts </Link> </p>
-                    <p className='hover:scale-105'> <Link to='/services/stationary/stationary' > Daily Stationery </Link> </p>
-                    <p className='hover:scale-105'> <Link to='/services/stationary/books_art' > Books & Art works </Link> </p>
-                    <p className='hover:scale-105'> <Link to='/services/stationary/personal' > Personal Stationery </Link> </p>
-                    <p className='hover:scale-105'> <Link to='/services/stationary' >Full Collection </Link> </p>
-                </div>
-            </div>
-
-            
-            <div className='flex flex-col rounded-md p-4'>
-                <h1 className='text-2xl font-semibold my-1 px-4 text-left ml-2 text-slate-700'>
-                    Facilities:
-                </h1>
-                <Link to='/services/facilities' > <img className='w-72 my-1 hover:scale-105 rounded-md mb-3 ' src="src\assets\partyhall.png" alt="" /> </Link>   
-                <div className='flex text-lg flex-col gap-2 font-semibold text-slate-100  *:min-w-40 *:bg-sky-400 *:rounded-tr-lg *:rounded-tl-lg *:p-1 '>
-                    <p className='hover:scale-105'> <Link to='/services/facilities/movie' > Movie Tickets </Link></p>
-                    <p className='hover:scale-105'> <Link to='/services/facilities/salon' > Beauty Salon </Link></p>
-                    <p className='hover:scale-105'> <Link to='/services/facilities/gym' > Fitness Center </Link></p>
-                    <p className='hover:scale-105'> <Link to='/services/facilities/partyhall' > Party Hall </Link></p>
-                    <p className='hover:scale-105'> <Link to='/services/facilities' > More Facilities </Link> </p>
-                </div>
-            </div>
-            
+    <div className="container mx-auto px-4 py-8">
+      <h1 className='text-5xl font-bold poppins text-blue-600 text-center mb-8'>
+        Services Available:
+      </h1>
+      
+      <div className='my-4 flex md:flex-row md:gap-16 flex-wrap justify-center poppins'>
+        <div className='flex flex-col items-center bg-white rounded-lg p-4 shadow-lg hover:shadow-2xl transition-shadow duration-300'>
+          <h1 className='text-2xl font-semibold my-1 px-4 text-center text-slate-700'>
+            Catering:
+          </h1>    
+          <Link to="/services/catering">
+            <img className='w-72 my-1 mb-3 hover:scale-105 rounded-md transition-transform duration-300' src="src/assets/food.png" alt="Catering Services" />
+          </Link>
+          <div className='flex text-lg flex-col gap-2 font-semibold text-rose-500  rounded-lg p-2'>
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to="/services/catering/snacks">  Snacks </Link> </p> 
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to="/services/catering/beverages">Beverages </Link> </p> 
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to="/services/catering/desserts">Desserts </Link> </p> 
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to="/services/catering/dining">Fine Dining </Link> </p> 
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to="/services/catering">Browse Menu </Link> </p> 
+          </div>
         </div>
-    </>
+
+        <div className='flex flex-col items-center bg-white rounded-lg p-4 shadow-lg hover:shadow-2xl transition-shadow duration-75'>
+          <h1 className='text-2xl font-semibold my-1 px-4 text-center text-slate-700'>
+            Stationery:
+          </h1>
+          <Link to='/services/stationary'>
+            <img className='w-72 my-1 rounded-md hover:scale-105 mb-3 transition-transform duration-75' src="src/assets/stationary.png" alt="Stationery Services" />
+          </Link>    
+          <div className='flex text-lg flex-col gap-2 font-semibold text-blue-500  rounded-lg p-2'>
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to='/services/stationary/gifts'> Gifts </Link> </p>
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to='/services/stationary/stationary'> Daily Stationery </Link> </p>
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to='/services/stationary/books_art'> Books & Art Works </Link> </p>
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to='/services/stationary/personal'> Personal Stationery </Link> </p>
+            <p className='hover:scale-105 transition-transform duration-75'> <Link to='/services/stationary'>Full Collection </Link> </p>
+          </div>
+        </div>
+
+        <div className='flex flex-col items-center bg-white rounded-lg p-4 shadow-lg hover:shadow-2xl transition-shadow duration-75'>
+          <h1 className='text-2xl font-semibold my-1 px-4 text-center text-slate-700'>
+            Facilities:
+          </h1>
+          <Link to='/services/facilities'>
+            <img className='w-72 my-1 hover:scale-105 rounded-md mb-3 transition-transform duration-75' src="src/assets/partyhall.png" alt="Facilities Services" />
+          </Link>   
+          <div className='flex text-lg flex-col gap-2 font-semibold text-green-500  rounded-lg p-2'>
+            <p className='hover:scale-105 transition-transform duration-100'> <Link to='/services/facilities/movie'> Movie Tickets </Link> </p>
+            <p className='hover:scale-105 transition-transform duration-100'> <Link to='/services/facilities/salon'> Beauty Salon </Link> </p>
+            <p className='hover:scale-105 transition-transform duration-100'> <Link to='/services/facilities/gym'> Fitness Center </Link> </p>
+            <p className='hover:scale-105 transition-transform duration-100'> <Link to='/services/facilities/partyhall'> Party Hall </Link> </p>
+            <p className='hover:scale-105 transition-transform duration-100'> <Link to='/services/facilities'> More Facilities </Link> </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Services
+export default Services;
--- FrontEnd/src/components/services/catering/Catering.jsx
+++ FrontEnd/src/components/services/catering/Catering.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FoodCard from './FoodCard'
+import {db} from '../../../firebase'
+import { getDocs, collection } from 'firebase/firestore'
 
 function Catering() {
+    const [loading, setLoading] = useState(true)
+    const [items, setItems] = useState(null)
+
+    useEffect( () => {
+        const fetchItems = async () =>{
+            const colRef = collection(db, 'Catering')
+            try{
+                const querySnap = await getDocs(colRef)
+                
+                const itemDocs = querySnap.docs.map((doc) =>
+                        ({id:doc.id,...doc.data()})
+                    )
+                setLoading(false)
+                    
+                setItems(itemDocs)
+                
+            }
+            catch(err){
+                console.log('Database error. : ',err);
+            }
+        }
+        fetchItems()
+    },[]
+    )
+    if(loading){
+        return <p> Loading.. </p>
+    }
+    
+
   return (
     <div className="p-6 bg-gradient-to-r from-blue-50 via-white to-blue-50 rounded-lg shadow-lg space-y-8">
         <h1 className="text-4xl font-bold text-center poppins text-gray-800">
@@ -30,9 +61,14 @@ function Catering() {
 
         {/* Food Items Grid */}
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            <FoodCard id="1" />
-            <FoodCard id="2" />
-            <FoodCard id="3" />
+            {
+                    items.map( (item)=> 
+                     <FoodCard key={item.id} name={item.name} desc={item.description} price={item.price} />
+
+                )
+            }
+            
+            
             {/* Add more FoodCard components as needed */}
         </div>
     </div>
