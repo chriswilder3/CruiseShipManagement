067544fd69e168cda8007c83b92a331611903df5
chriswilder3 <chriswilderdelta19@gmail.com>
2025-01-03 16:07:12 +0530

    added fitness comp & its card comp

---
+++ FrontEnd/src/components/services/facilities/fitness/FitnessCard.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+function FitnessCard({itemId, name, desc, price, duration, imageUrl}) {
+
+  const handleAddCart = () => {
+
+  }
+    
+  return (
+    <div className='flex flex-col p-5 bg-slate-200 rounded-md '>
+        <img src={imageUrl} className='w-24 rounded self-center' alt="" />
+        
+        {/* Saloon service name */}
+        <h1 className='p-1 text-gray-700'>
+            {name}
+        </h1>
+
+        {/* Saloon service description */}
+        <p className="text-sm text-gray-600 text-center mb-2">
+            {desc.length > 100 ? `${description.substring(0, 100)}...` : desc}
+        </p>
+
+        {/* Saloon service duration */}
+        <p className="text-sm text-gray-500 text-center mb-2">Duration: {duration}</p>
+
+        {/* price */}
+        <p className="text-lg font-medium mb-2 text-green-500">₹{price}</p>
+        
+        <button onClick={handleAddCart} id="order"
+                className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition text-sm">
+            Book Now
+        </button>
+
+    </div>
+  )
+}
+
+export default FitnessCard
