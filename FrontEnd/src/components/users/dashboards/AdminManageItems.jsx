690f755001a339a7f6f7924fc70e00ddaedbba89
chriswilder3 <chriswilderdelta19@gmail.com>
2025-01-05 19:47:30 +0530

    improved adminadditem page

--- FrontEnd/src/components/users/dashboards/AdminManageItems.jsx
+++ FrontEnd/src/components/users/dashboards/AdminManageItems.jsx
@@ -9,12 +9,9 @@ function AdminManageItems() {
     setSelectedService(selectedValue);
 
     switch (selectedValue) {
-
-      
-       case "Catering":
+      case "Catering":
         setFormFields([
           { name: "itemName", label: "Item Name", type: "text" },
-          { name: "itemType", label: "Item Type", type: "select", options: ["Snacks", "Beverages", "Desserts", "Fine Dining"] },
           { name: "price", label: "Price", type: "number" },
           { name: "description", label: "Description", type: "textarea" },
           { name: "imageUrl", label: "Image URL", type: "text" },
@@ -24,7 +21,6 @@ function AdminManageItems() {
       case "Stationery":
         setFormFields([
           { name: "itemName", label: "Item Name", type: "text" },
-          { name: "itemType", label: "Item Type", type: "select", options: ["Gifts", "Daily Stationery", "Books & Art works", "Personal Stationery"] },
           { name: "price", label: "Price", type: "number" },
           { name: "description", label: "Description", type: "textarea" },
           { name: "imageUrl", label: "Image URL", type: "text" },
@@ -39,7 +35,7 @@ function AdminManageItems() {
           { name: "posterUrl", label: "Poster URL", type: "text" },
         ]);
         break;
-        
+
       case "Beauty Salon":
         setFormFields([
           { name: "serviceName", label: "Service Name", type: "text" },
@@ -71,7 +67,7 @@ function AdminManageItems() {
           {
             name: "packageCategory",
             label: "Package Category",
-            type: "checkbox",
+            type: "select",
             options: [
               "Birthday Package",
               "Wedding Package",
@@ -80,8 +76,6 @@ function AdminManageItems() {
           },
         ]);
         break;
-
-      
 
       default:
         setFormFields([]);
@@ -102,28 +96,6 @@ function AdminManageItems() {
               className="w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
               placeholder={`Enter ${field.label}`}
             ></textarea>
-          </div>
-        );
-      } else if (field.type === "checkbox") {
-        return (
-          <div key={field.name} className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">{field.label}</label>
-            <div className="space-y-2">
-              {field.options.map((option, index) => (
-                <div key={index} className="flex items-center space-x-2">
-                  <input
-                    type="checkbox"
-                    id={`${field.name}_${index}`}
-                    name={field.name}
-                    value={option}
-                    className="h-4 w-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
-                  />
-                  <label htmlFor={`${field.name}_${index}`} className="text-sm text-gray-600">
-                    {option}
-                  </label>
-                </div>
-              ))}
-            </div>
           </div>
         );
       } else if (field.type === "select") {
@@ -167,41 +139,61 @@ function AdminManageItems() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-800 to-purple-900 text-white">
-      <div className=" max-w-4xl p-3 ">
-        <h1 className="text-3xl font-bold mb-6 roboto-slab text-indigo-300">Manage all Items here</h1>
+      <div className="p-3 w-3/4">
+        
+        <h1 className="text-3xl font-bold mb-6 poppins text-indigo-300">Manage Items/Services </h1>
 
-        <div className="p-3 bg-white rounded-lg shadow-lg text-gray-900">
-          <h2 className="text-lg font-semibold mb-4">Add New Item</h2>
-          <form>
-            <div className="mb-6">
-              <label htmlFor="servicetype" className="block text-sm font-medium text-gray-700 mb-1">
-                Type of Service
-              </label>
-              <select
-                name="servicetype"
-                id="servicetype"
-                onChange={handleServiceChange}
-                className="w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-              >
-                <option value="">Select a service</option>
-                <option value="Beauty Salon">Beauty Salon</option>
-                <option value="Fitness Center">Fitness Center</option>
-                <option value="Partyhall">Partyhall</option>
-                <option value="Catering">Catering</option>
-                <option value="Stationery">Stationery</option>
-                <option value="Movies">Movies</option>
-              </select>
+          
+        <div className="flex flex-col space-y-4 w-3/4 justify-center mx-auto items-center">
+          
+            <div className="flex flex-col space-y-4 w-1/3">
+                <button
+                  className="w-full py-2 px-4 bg-red-500 text-white rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+                >
+                  Delete Existing Items
+                </button>
+                <button
+                  className="w-full py-2 px-4 bg-yellow-500 text-white rounded-md shadow-sm hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                >
+                  Update Existing Items
+                </button>
             </div>
 
-            <div>{renderFormFields()}</div>
+          <div className="p-3 bg-white rounded-lg shadow-lg text-gray-900 w-4/5">
+            <h2 className="text-lg font-semibold mb-4 text-sky-600">Add New Item</h2>
+            <form>
+              <div className="mb-6">
+                <label htmlFor="servicetype" className="block text-sm font-medium text-gray-700 mb-1">
+                  Type of Service
+                </label>
+                <select
+                  name="servicetype"
+                  id="servicetype"
+                  onChange={handleServiceChange}
+                  className="w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                >
+                  <option value="">Select a service</option>
+                  <option value="Beauty Salon">Beauty Salon</option>
+                  <option value="Fitness Center">Fitness Center</option>
+                  <option value="Partyhall">Partyhall</option>
+                  <option value="Catering">Catering</option>
+                  <option value="Stationery">Stationery</option>
+                  <option value="Movies">Movies</option>
+                </select>
+              </div>
 
-            <button
-              type="submit"
-              className="w-full py-2 px-4 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            >
-              Submit
-            </button>
-          </form>
+              <div>{renderFormFields()}</div>
+
+              <button
+                type="submit"
+                className="w-full py-2 px-4 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+
+          
         </div>
       </div>
     </div>
